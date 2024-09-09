4bb956143c6c5d2750ac255e71e1df887d01cdf4
Mouzone <sunnyliu010@gmail.com>
2024-09-09 17:48:10 -0400

    Create html structure of resume to be displayed

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,18 +1,39 @@
 export default function Resume({ personal, education, experience }) {
     return (
         <>
-            <div id="header">
+            <div id="personal">
                 <h1> { personal["name"] }</h1>
                 <div className="personalBar">
-                    <p> { personal["email"] }</p>
-                    <p> { personal["phone"] }</p>
-                    <p> { personal["address"] }</p>
+                    <p> { personal["email"] } </p>
+                    <p> { personal["phone"] } </p>
+                    <p> { personal["address"] } </p>
                 </div>
             </div>
-            <div id="Education">
+            <div id="education">
                 <h2> Education </h2>
-                <div className="left">
-                    <p> </p>
+                <div className="item">
+                    <div className="left">
+                        <p> { education["start"] } - { education["end"] } </p>
+                        <p> { education["location"] } </p>
+                    </div>
+                    <div className="right">
+                        <p> { education["school"] } </p>
+                        <p> { education["degree"] } </p>
+                    </div>
+                </div>
+            </div>
+            <div id="experience">
+                <h2> Professional Experience </h2>
+                <div className="item">
+                    <div className="left">
+                        <p> { experience["start"] } - { experience["left"] } </p>
+                        <p> { experience["location"] } </p>
+                    </div>
+                    <div className={"right"}>
+                        <p> { experience["company"] } </p>
+                        <p> { experience["position"] } </p>
+                        <p> { experience["description"] } </p>
+                    </div>
                 </div>
             </div>
         </>
