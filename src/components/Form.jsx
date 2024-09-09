a9bcda6aed12731a91976744032cf2d6b3336845
Mouzone <sunnyliu010@gmail.com>
2024-09-09 15:34:53 -0400

    Spacing around brackets

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,167 +1,167 @@
-function Personal( {state, setState} ) {
+function Personal({ state, setState }) {
     function changeName(e) {
-        setState( {...state, name: e.target.value} )
+        setState({ ...state, name: e.target.value })
     }
 
     function changeEmail(e) {
-        setState( {...state, email: e.target.value} )
+        setState({ ...state, email: e.target.value })
     }
 
     function changePhone(e) {
-        setState( {...state, phone: e.target.value} )
+        setState({ ...state, phone: e.target.value })
     }
 
     function changeAddress(e) {
-        setState( {...state, address: e.target.value} )
+        setState({ ...state, address: e.target.value })
     }
 
     return (
         <>
             <Input
                 label="Full Name"
-                value={state["name"]}
-                handleChange={changeName}
+                value={ state["name"] }
+                handleChange={ changeName }
                 />
             <Input
                 label="Email"
-                value={state["email"]}
-                handleChange={changeEmail}
+                value={ state["email"] }
+                handleChange={ changeEmail }
                 />
             <Input
                 label="Phone Number"
-                value={state["phone"]}
-                handleChange={changePhone}
+                value={ state["phone"] }
+                handleChange={ changePhone }
                 />
             <Input
                 label="Address"
-                value={state["address"]}
-                handleChange={changeAddress}
+                value={ state["address"] }
+                handleChange={ changeAddress }
                 />
         </>
     )
 }
 
-function Education( {state, setState} ) {
+function Education({ state, setState }) {
     function changeSchool(e) {
-        setState( {...state, school: e.target.value} )
+        setState({ ...state, school: e.target.value })
     }
 
     function changeDegree(e) {
-        setState( {...state, degree: e.target.value} )
+        setState({ ...state, degree: e.target.value })
     }
 
     function changeStart(e) {
-        setState( {...state, start: e.target.value} )
+        setState({ ...state, start: e.target.value })
     }
 
     function changeEnd(e) {
-        setState( {...state, end: e.target.value} )
+        setState({ ...state, end: e.target.value })
     }
 
     function changeLocation(e) {
-        setState( {...state, location: e.target.value})
+        setState({ ...state, location: e.target.value })
     }
 
     return (
         <>
             <Input
                 label="School"
-                value={state["school"]}
-                handleChange={changeSchool}
+                value={ state["school"] }
+                handleChange={ changeSchool }
             />
             <Input
                 label="Degree"
-                value={state["degree"]}
-                handleChange={changeDegree}
+                value={ state["degree"] }
+                handleChange={ changeDegree }
             />
             <Input
                 label="Start"
-                value={state["start"]}
-                handleChange={changeStart}
+                value={ state["start"] }
+                handleChange={ changeStart }
             />
             <Input
                 label="End"
-                value={state["end"]}
-                handleChange={changeEnd}
+                value={ state["end"] }
+                handleChange={ changeEnd }
             />
             <Input
                 label="Location"
-                value={state["location"]}
-                handleChange={changeLocation}
+                value={ state["location"] }
+                handleChange={ changeLocation }
             />
         </>
     )
 }
 
-function Experience( {state, setState} ) {
+function Experience({ state, setState }) {
     function changeCompany(e) {
-        setState( {...state, company: e.target.value} )
+        setState({ ...state, company: e.target.value })
     }
 
     function changePosition(e) {
-        setState( {...state, position: e.target.value} )
+        setState({ ...state, position: e.target.value })
     }
 
     function changeStart(e) {
-        setState( {...state, start: e.target.value} )
+        setState({ ...state, start: e.target.value })
     }
 
     function changeEnd(e) {
-        setState( {...state, end: e.target.value} )
+        setState({ ...state, end: e.target.value })
     }
 
     function changeLocation(e) {
-        setState( {...state, location: e.target.value} )
+        setState({ ...state, location: e.target.value })
     }
 
     function changeDescription(e) {
-        setState( {...state, description: e.target.value})
+        setState({ ...state, description: e.target.value })
     }
 
     return (
         <>
             <Input
                 label="Company Name"
-                value={state["company"]}
-                handleChange={changeCompany}
+                value={ state["company"] }
+                handleChange={ changeCompany }
             />
             <Input
                 label="Position"
-                value={state["position"]}
-                handleChange={changePosition}
+                value={ state["position"] }
+                handleChange={ changePosition }
             />
             <Input
                 label="Start Date"
-                value={state["start"]}
-                handleChange={changeStart}
+                value={ state["start"] }
+                handleChange={ changeStart }
             />
             <Input
                 label="End Date"
-                value={state["end"]}
-                handleChange={changeEnd}
+                value={ state["end"] }
+                handleChange={ changeEnd }
             />
             <Input
                 label="Location"
-                value={state["location"]}
-                handleChange={changeLocation}
+                value={ state["location"] }
+                handleChange={ changeLocation }
             />
             <Input
                 label="Description"
-                value={state["description"]}
-                handleChange={changeDescription}
+                value={ state["description"] }
+                handleChange={ changeDescription }
             />
         </>
     )
 }
 
-function Input( {label, value, handleChange} ) {
+function Input({ label, value, handleChange }) {
     return (
         <label>
-            {label}
-            {' '}
+            { label }
+            { ' ' }
             <input
-                value={value}
-                onChange={handleChange}
+                value={ value }
+                onChange={ handleChange }
                 />
         </label>
     )
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,12 @@
-export default function Resume( {personal, education, experience} ) {
+export default function Resume({ personal, education, experience }) {
     return (
         <>
             <div id="header">
-                <h1> {personal["name"]}</h1>
+                <h1> { personal["name"] }</h1>
                 <div className="personalBar">
-                    <p> {personal["email"]}</p>
-                    <p> {personal["phone"]}</p>
-                    <p> {personal["address"]}</p>
+                    <p> { personal["email"] }</p>
+                    <p> { personal["phone"] }</p>
+                    <p> { personal["address"] }</p>
                 </div>
             </div>
             <div id="Education">
