3c6a84bbffe33ea740f25b186362e2f4f7a1bfae
Mouzone <sunnyliu010@gmail.com>
2024-09-11 19:22:30 -0400

    Add ternary operator for header icons for input sections

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -93,15 +93,21 @@ function StandardInputs({ isEducation, state, setState }) {
         <div id={ isEducation ? "educationInput" : "experienceInput" }>
             <div className="top">
                 <h2>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path
-                            d="M10,2H14A2,2 0 0,1 16,4V6H20A2,2 0 0,1 22,8V19A2,2 0 0,1 20,21H4C2.89,21 2,20.1 2,19V8C2,6.89 2.89,6 4,6H8V4C8,2.89 8.89,2 10,2M14,6V4H10V6H14Z"/>
-                    </svg>
-                    { isEducation ? "Education" : "Professional Experience"}
+                    {isEducation
+                        ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path
+                                d="M12 3L1 9L5 11.18V17.18L12 21L19 17.18V11.18L21 10.09V17H23V9L12 3M18.82 9L12 12.72L5.18 9L12 5.28L18.82 9M17 16L12 18.72L7 16V12.27L12 15L17 12.27V16Z"/>
+                        </svg>
+                        : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path
+                                d="M10,2H14A2,2 0 0,1 16,4V6H20A2,2 0 0,1 22,8V19A2,2 0 0,1 20,21H4C2.89,21 2,20.1 2,19V8C2,6.89 2.89,6 4,6H8V4C8,2.89 8.89,2 10,2M14,6V4H10V6H14Z"/>
+                        </svg>
+                    }
+                    {isEducation ? "Education" : "Professional Experience"}
                 </h2>
-                <button onClick={ changeShow } style={ { background: 'none', border: 'none', cursor: 'pointer' } }>
-                    { show ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <button onClick={changeShow} style={{background: 'none', border: 'none', cursor: 'pointer'}}>
+                    {show ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M7,15L12,10L17,15H7Z"/>
                             </svg>
                         ) : (
